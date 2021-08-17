b128d1458f8d43ffcd7429ccead3bb42f20e7877
SHAURYA <85989640+shaurya2001@users.noreply.github.com>
2021-08-17 16:43:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var tower;
+var canvas,angle,tower,ground,cannon;
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+function setup() {
+ canvas=createCanvas(1200,600);
+ engine = Engine.create();
+  world = engine.world;
+  angle=-PI/4;
+  //ground=new Ground(0,height -1, widht * 2,1);
+  cannon= new Cannon(180,110,108,50,angle);
+  tower = new Tower(150, 350, 160, 310);
+
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+function draw() 
+{
+  background(189);
+  image(backgroundImg,0,0,width,height);
+
+
+  Engine.update(engine);
+    tower.display();
+    //ground.display();
+    cannon.display();
+
+ 
+}
+
